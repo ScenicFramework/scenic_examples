e08b393527779ca1e7e3ee8dc82e82d29ecf5e57
crertel <chris@kedagital.com>
2021-07-11 01:49:48 -0500

    Start stubbing in primitives example.

---
+++ primitives/README.md
@@ -0,0 +1,5 @@
+# Scenic Examples: Primitives
+
+This project is meant to show all of the drawing primitives Scenic is capable of.
+
+Things to note:
---
+++ primitives/config/config.exs
@@ -0,0 +1,25 @@
+use Mix.Config
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# should normally live in config.exs of the device app
+config :scenic, :assets,
+  module: Example.Assets,
+  alias: []
+
+config :logger, :console, format: "[$level] $message\n"
+
+# Configure the main viewport for the Scenic application
+config :example, :viewport, [
+    name: :main_viewport,
+    size: {800, 600},
+    default_scene: Example.Scene,
+    drivers: [[
+            module: Scenic.Driver.Glfw,
+            name: :glfw_driver,
+            title: "Primitives Example"
+          ]]
+    ]
