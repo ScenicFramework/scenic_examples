e0234c13e2f3b84980b93d1cc27cc7ac206ba205
crertel <chris@kedagital.com>
2021-07-17 01:19:33 -0500

    Add sprites demo.

---
+++ sprites/README.md
@@ -0,0 +1,9 @@
+# Scenic Examples: Sprites
+
+This project shows how to use sprites. It uses an example sprite sheet from OpenGameArt.org, available [here](https://opengameart.org/content/weird-fruits-16x16).
+
+Things to note:
+
+* It's totally fine to describe a destination rectangle smaller or larger than the source region on the sprite.
+* Unlike most other primitive commands, currently no transforms or styles are applied to sprites.
+* If you want to transform sprites, you'll need to use groups.
---
+++ sprites/lib/scenes/scene.ex
@@ -0,0 +1,44 @@
+defmodule Example.Scene do
+  @moduledoc """
+  This is a minimal scene with some text.
+  """
+  use Scenic.Scene
+
+  alias Scenic.Graph
+
+  import Scenic.Primitives
+
+  def init(scene, _param, _opts) do
+    {vp_width, _} = scene.viewport.size
+
+    # Note that sprites/3 doesn't support styles or transforms, so we do that with group/2
+    graph = Graph.build()
+            |> sprites( { "sprites/fruit.png", [
+              {{0,0}, {80, 80}, {0, 0}, {100, 100}},
+              {{80,0}, {80, 80}, {80, 0}, {100, 100}},
+              {{160,0}, {80, 80}, {160, 0}, {100, 100}},
+              {{240,0}, {80, 80}, {240, 0}, {100, 100}},
+
+              {{0,80}, {80, 80}, {0, 80}, {100, 100}},
+              {{80,80}, {80, 80}, {80, 80}, {100, 100}},
+              {{160,80}, {80, 80}, {160, 80}, {100, 100}},
+              {{240,80}, {80, 80}, {240, 80}, {100, 100}},
+            ]})
+            |> group( fn(g) ->
+              g
+              |> sprites( { "sprites/fruit.png", [
+                {{0,160}, {80, 80}, {0, 160}, {100, 100}},
+                {{80,160}, {80, 80}, {80, 160}, {100, 100}},
+                {{160,160}, {80, 80}, {160, 160}, {100, 100}},
+                {{240,160}, {80, 80}, {240, 160}, {100, 100}},
+
+                {{0,240}, {80, 80}, {0, 240}, {100, 100}},
+                {{80,240}, {80, 80}, {80, 240}, {100, 100}},
+                {{160,240}, {80, 80}, {160, 240}, {100, 100}},
+                {{240,240}, {80, 80}, {240, 240}, {100, 100}},
+              ]})
+            end, translate: {200,0},rotate: 25* (:math.pi() / 180.0))
+
+    { :ok, push_graph( scene, graph ) }
+  end
+end
