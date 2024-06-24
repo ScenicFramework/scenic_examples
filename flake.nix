c5f1c35ff0be386f11c05138e3cb6ca859e9a726
crertel <chris@kedagital.com>
2024-06-24 00:32:28 -0500

    Add development flake.

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Flake for building scenic examples.";
+
+  inputs = {
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-23.11"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        inherit (pkgs.lib) optional optionals;
+        pkgs = import nixpkgs { inherit system; };
+
+        elixir = pkgs.beam.packages.erlang.elixir;
+      in
+      with pkgs;
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            util-linux
+            libselinux
+            libthai
+            libdatrie
+            libsepol
+            libxkbcommon
+            libepoxy
+            pcre
+            pcre2
+            xorg.libXtst
+            cairo
+            gtk3
+            freeglut
+            elixir_1_16
+            elixir_ls
+            glibcLocales
+            glew
+            glfw
+            pkg-config
+            xorg.libX11
+            xorg.libXau
+            xorg.libXdmcp
+          ] ++ optional stdenv.isLinux inotify-tools
+          ++ optional stdenv.isDarwin terminal-notifier
+          ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+            CoreFoundation
+            CoreServices
+          ]);
+        };
+      });
+}
